403af3f7b4a10dbb6f24311d64139248a9eac4a1
karan <karan@HGBAUN4.local>
2019-01-09 16:10:00 +0530

    Updates

---
+++ precache-manifest.a4cf777f3bbc784cb115503dce1a2db8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d9e4870df35f98c4150",
+    "url": "/portfolio/static/css/main.749ef763.chunk.css"
+  },
+  {
+    "revision": "5d9e4870df35f98c4150",
+    "url": "/portfolio/static/js/main.5d9e4870.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/portfolio/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b5e7130f274fe7bdacfb23527e025a2a",
+    "url": "/portfolio/index.html"
+  }
+];
